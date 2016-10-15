f51fc28b585f6f659fd65958a8fffb1987f59096
giuseppec <giuseppe.casalicchio@gmail.com>
2016-10-15 07:00:17 +0200

    add test and improve docu

--- R/ResampleResult_operators.R
+++ R/ResampleResult_operators.R
@@ -29,58 +29,78 @@ getRRTaskDescription = function(res) {
   res$task.desc
 }
 
+# This function creates Prediction objects for each resample iteration (separately, for train and test set)
 getPredictionList = function(res, ...) {
   assertClass(res, "ResampleResult")
-
-  predict.type = res$pred$predict.type
+  # We need to force keep.pred = TRUE (will be checked in getRRPredictions)
+  pred = getRRPredictions(res)
+  predict.type = pred$predict.type
+  time = pred$time
   task.desc = getRRTaskDescription(res)
-  time = res$pred$time #res$runtime/res$pred$instance$desc$iters
 
   # split by train and test set
-  set = unique(res$pred$data$set)
+  set = levels(pred$data$set)
 
-  # get prediction object for train and test set
+  # get prediction objects for train and test set
   prediction = lapply(set, function(s) {
-    pred = subset(res$pred$data, set == s)
-    pred.split = split(pred, as.factor(pred$iter))
     # split by resample iterations
-    prediction = lapply(pred.split, function (pred) {
+    p.split = subset(pred$data, set == s)
+    p.split = split(p.split, as.factor(p.split$iter))
+    # create prediction object for each resample iteration
+    p.split = lapply(p.split, function (p) {
       # get predictions based on predict.type
       if (predict.type == "prob") {
-        y = pred[,grepl("^prob[.]", colnames(pred))]
+        y = p[,grepl("^prob[.]", colnames(p))]
+        # we need to remove the "prob." part in the colnames, otherwise
+        # makePrediction thinks that the factor starts with "prob."
         colnames(y) =  gsub("^prob[.]", "", colnames(y))
       } else {
-        y = pred$response
+        y = p$response
       }
-      makePrediction(task.desc, id = pred$id,
-        truth = pred$truth, y = y, row.names = pred$id,
+      makePrediction(task.desc, id = p$id,
+        truth = p$truth, y = y, row.names = p$id,
         predict.type = predict.type, time = NA, ...)
     })
-    # add time info
-    for(i in 1:length(prediction)) prediction[[i]]$time = time[i]
-    return(prediction)
+    # add time info afterwards
+    for(i in 1:length(p.split)) p.split[[i]]$time = time[i]
+    return(p.split)
   })
-  setNames(prediction, set)
+  ret = setNames(prediction, set)
+  if (is.null(ret$train)) ret = append(ret, list(train = NULL))
+  if (is.null(ret$test)) ret = append(ret, list(test = NULL))
+  return(ret[c("train", "test")])
 }
 
+#' @title Compute new measures for existing ResampleResult
+#' @description
+#'  Adds new measures to an existing \code{ResampleResult}.
+#' @param res [\code{ResampleResult}]\cr
+#'   The result of \code{\link{resample}} run with \code{keep.pred = TRUE}.
+#' @template arg_measures
+#' @return [\code{\link{ResampleResult}}].
+#' @export
+#' @family resample
 addRRMeasure = function(res, measures) {
   assertClass(res, "ResampleResult")
   if (inherits(measures, "Measure")) measures = list(measures)
 
+  # check if measures are missing in ResampleResult object
   measures.id = vcapply(measures, function(x) x$id)
   missing.measures = setdiff(measures.id, colnames(res$measures.test))
+
   # if there are missing measures
   if (length(missing.measures) != 0) {
     # get list of prediction objects per iteration from resample result
     pred = getPredictionList(res)
-    set = names(pred)
 
+    # recompute missing performance for train and/or test set
+    set = names(pred)[!vlapply(pred, is.null)]
     perf = setNames(lapply(set, function(s) {
       as.data.frame(do.call("rbind", lapply(pred[[s]], function(p) {
         ret = performance(p, measures)
         matrix(ret, ncol = length(measures), dimnames = list(NULL, names(ret)))
       })))
-    }), names(pred))
+    }), set)
 
     # add missing measures to resample result
     if (is.null(perf$train))
